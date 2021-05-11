ecd68da1141308acff117c810c94e08ce1deb294
mingubeep <bramurve@gmail.com>
2021-05-11 16:32:34 +0000

    add flask to use  html

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
 
 app = Flask("SuperScrapper")
 
+db = {}
+
+
 @app.route("/")
-
 def home():
-  return "Hello! welcome to mi casa!"
+    return render_template("home.html")
 
-@app.route("/contact")
-def contact():
-  return "Contact me!"
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        redirect("/")
+    return render_template("report.html",
+                           resultNumber=len(jobs),
+                           searchingBy=word,
+                           jobs=jobs)
+
 
 app.run(host="0.0.0.0")
