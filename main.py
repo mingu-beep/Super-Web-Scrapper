db3fe365e874ce8f694aba3127b8c1094080a14d
mingubeep <>
2021-05-11 14:46:29 +0000

    fisrt commit of this repo

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+
+def home():
+  return "Hello! welcome to mi casa!"
+
+@app.route("/contact")
+def contact():
+  return "Contact me!"
+
+app.run(host="0.0.0.0")
